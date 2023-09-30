82b79603a4a53225d358b95dcfdf2a2850f1d234
htqnhat280203 <htqnhat280203@gmail.com>
2023-09-30 11:06:25 +0700

    first commit

---
+++ src/view/Main.java
@@ -0,0 +1,26 @@
+package view;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import controller.ManagerMenu;
+import controller.ManagerMenu;
+import model.Manager;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       ManagerMenu model = new ManagerMenu();
+        model.run();
+    }
+
+}
